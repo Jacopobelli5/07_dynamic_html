3e298f1f86df0623e6c6971500b217f4f0cae9a9
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-13 23:54:04 +0100

    added ejs tags

--- routes/main.js
+++ routes/main.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
-router.get("/", (req, res) => res.render("index.ejs"));
-router.get("/about", (req, res) => res.render("about.ejs"));
+router.get("/", (req, res) => res.render("index.ejs", {title: "Dynamic Title", header: "This is the Homepage"}));
+router.get("/about", (req, res) => res.render("about.ejs", {aboutTitle: "About"}));
 router.get("/contact", (req, res) => res.render("contact.ejs"))
 
 // Export the router object
